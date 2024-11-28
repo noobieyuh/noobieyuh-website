3709c52c6f1cd9f929d2b7cca0a0c19d3390ef25
noobie <156363695+noobieyuh@users.noreply.github.com>
2024-11-28 00:13:59 +0000

    more updates yayy

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function checkGitHub()
             document.getElementById("thanks").style.visibility = "visible";
         }
 }
+
+function clickMe()
+{
+    var secretSound = new Audio("assets/audio/evillaugh.wav")
+    secretSound.play();
+    document.getElementById("pressed").style.visibility = "visible";
+}
